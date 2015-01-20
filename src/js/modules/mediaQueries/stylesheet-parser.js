90ac931ece54cae81d0a4abb8b78604a97a0ada4
Ellen Schwartau <eschwartau@frontlineshop.com>
2015-01-20 16:58:14 +0100

    Umbau StyleSheet parser, merkt sich jetzt auch den Index des StyleSheets und den der CSSRegel, um diese später bearbeiten zu können

--- src/js/modules/mediaQueries/stylesheet-parser.js
+++ src/js/modules/mediaQueries/stylesheet-parser.js
@@ -10,80 +10,83 @@ function($){
     var CSS_MEDIA_RULE_TYPE = 4;
 
     /**
-     * Liefert alle css-Regeln der aktuell geladenen Stylesheets.
+     * Liefert alle css-Regeln eines bestimmten Typs der aktuell geladenen Stylesheets.
      * @returns {Array}
      */
-    var getCssRules = function() {
+    var getCssRules = function(type) {
         var styleSheets = document.styleSheets,
             cssRules = [];
-        $.each(styleSheets, function(i, styleSheet){
-            var ruleLists = styleSheet.cssRules;
-            if(ruleLists != null && ruleLists.length > 0) {
-                $.each(ruleLists, function(i, rules){
-                    cssRules.push(rules);
-                });
+
+        // StyleSheets durchlaufen
+        $.each(styleSheets, function(indexStyleSheet, styleSheet){
+            var ruleList = styleSheet.cssRules;
+            if(ruleList != null && ruleList.length > 0) {
+                // Für jedes StyleSheet die Regeln speichern
+                cssRules.push(getCssRulesFromRuleList(type, indexStyleSheet, ruleList));
             }
         });
-        // TODO hier evtl. schon in JSON Format konvertieren und Index des Stylesheets, sowie der Rule mit speichern
-        // um im Nachhinein das bearbeiten zu ermöglichen
+
         return cssRules;
     };
 
     /**
-     * Liefert alle css-Regeln eines bestimmten Typs.
-     * @param type  int Typ der gesuchten Regeln
-     */
-    var getCssRulesWithType = function(type) {
-        var cssRules = getCssRules(),
-            filteredRules = [];
-        $.each(cssRules, function(i, cssRule) {
-            if(cssRule.type == type) {
-                filteredRules.push(cssRule);
-            }
-        });
-        return filteredRules;
-    };
-
-    /**
-     * Filtert aus css-Regeln eines bestimmten Typs diejenigen heraus,
-     * deren definiertes Attribut einen bestimmten Wert enthält.
-     * @param type              Typ der Css-Regeln, die durchsucht werden sollen
-     * @param attribute         Attribut, das einen bestimmten Wert enthalten soll
-     * @param shouldContain     Wert, der im Attribut enthalten sein soll
+     * List aus einer Liste an CSSRules die einzelnen Regeln aus und konvertiert die
+     * benötigten Informationen ins JSON-Format.
+     * @param type              int             Typ der Regeln, die geliefert werden sollen
+     * @param indexStyleSheet   int             Index des Style Sheets, aus dem die Media Query stammt
+     * @param ruleList          CSSRuleList     Liste, die die css-Regeln enthält
      * @returns {Array}
      */
-    var filterCssRule = function(type, attribute, shouldContain) {
-        var cssRules = getCssRulesWithType(type),
-            filteredRules = [];
-        $.each(cssRules, function(i, cssRule){
-            var value = cssRule[attribute];
-            if(value !== undefined) {
-                if(value.indexOf(shouldContain) > -1) {
-                    filteredRules.push(cssRule);
-                }
+    var getCssRulesFromRuleList = function(type, indexStyleSheet, ruleList) {
+        var cssRules = [];
+        $.each(ruleList, function(indexRule, rule){
+            if(rule.type == type) {
+                // Jede Rule des Typs in JSON mit den benötigten Informationen speichern
+                cssRules.push(convertRuleToJson(type, indexStyleSheet, indexRule, rule));
             }
         });
-        return filteredRules;
+        return cssRules;
     };
 
     /**
-     * Liefert den Wert des Selektors.
-     * @param rule  CSSRule
+     * Konvertiert eine Media Query in ein JSON-Format.
+     * @param type              int     Typ der Regeln, die geliefert werden sollen
+     * @param indexStyleSheet   int         Index des Style Sheets, aus dem die Media Query stammt
+     * @param indexRule         int         Index der Regel, die die Media Query definiert
+     * @param rule              CSSRegel    Regel
+     * @returns {{indexStyleSheet: *, indexRule: *, fullCss: (string|showComposition.$composition.cssText|*|rule.cssText|cssText|.style.cssText), selector: String}}
+     */
+    var convertRuleToJson = function(type, indexStyleSheet, indexRule, rule) {
+        return addTypeSpecificInformation(type, rule, {
+            indexStyleSheet: indexStyleSheet,
+            indexRule: indexRule,
+            fullCss: rule.cssText
+        });
+    };
+
+    /**
+     * Fügt dem JSON Object der css-Regel je nach dessen Typ weitere Daten an.
+     * @param type      int     Typ der css-Regel
+     * @param rule      CSSRule css-Regel
+     * @param json      JSON    bisherig gesammelte Daten zur Regel
+     */
+    var addTypeSpecificInformation = function(type, rule, json) {
+        switch(type) {
+            case CSS_MEDIA_RULE_TYPE:
+                json.mediaQueryWidth = getMediaQueryWidth(rule);
+                json.selector = getMediaRuleSelectorValue(rule);
+                break;
+        }
+        return json;
+    };
+
+    /**
+     * Liefert den Wert des Selektors einer Media Query.
+     * @param rule      CSSMediaRule
      * @returns String
      */
-    var getSelectorValue = function(rule) {
+    var getMediaRuleSelectorValue = function(rule) {
         return "@media " + rule.media.mediaText;
-    };
-
-    /**
-     * Liefert die Styles einer CSSRule inklusive geschweiften Klammern.
-     * @param rule          CSSRule
-     * @param selector      String  Selektor
-     * @returns {string}
-     */
-    var getStyleValue = function(rule, selector) {
-        var cssText = rule.cssText;
-        return cssText.replace(selector, "");
     };
 
     /**
@@ -124,44 +127,13 @@ function($){
     };
 
     /**
-     * Formatiert Styleangaben und fügt Absätze ein.
-     * @param style
-     */
-    var formatStyle = function(style) {
-        return style
-            .replace(/\{/g, "{<br>")
-            .replace(/;/g, ";<br>")
-            .replace(/\}/g, "}<br>");
-    };
-
-    /**
-     * Konvertiert die Regeln und die notwendigen Informationen in JSON.
-     * @param cssRules  []  CSSRules
-     * return {selector: String, style: String}
-     */
-    var convertRulesToJSON = function(cssRules){
-        var jsonRules = [];
-        $.each(cssRules, function(i, rule){
-            var selector = getSelectorValue(rule),
-                style = getStyleValue(rule, selector),
-                json = {
-                    selector: getSelectorValue(rule),
-                    style: formatStyle(style),
-                    mediaQueryWidth: getMediaQueryWidth(rule),
-                    fullCss: rule.cssText
-                };
-            jsonRules.push(json);
-        });
-        return jsonRules;
-    };
-
-    /**
      * Liefert die CSS-Rules, die @media-Angaben enthalten.
      */
     var getMediaQueries = function(){
-        return convertRulesToJSON(
-            getCssRulesWithType(CSS_MEDIA_RULE_TYPE)
-        );
+        //return convertRulesToJSON(
+        //    getCssRulesWithType(CSS_MEDIA_RULE_TYPE)
+        //);
+        console.log(getCssRules(CSS_MEDIA_RULE_TYPE));
     };
 
     return {
