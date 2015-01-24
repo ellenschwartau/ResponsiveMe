2bbf204b89093be8276b4edb9a360a1414e57a52
ellenschwartau <ellen.schwartau@hotmail.de>
2015-01-24 11:07:01 +0100

    Umbenannt handleShowGrid in handleShowElements

--- src/js/background/background.js
+++ src/js/background/background.js
@@ -3,30 +3,30 @@
  * Sie läuft durchgehend im Hintergrund und hält Daten und Funktionen zur Kommunikation zwischen
  * den einzelnen Bestandteilen der Erweiterung bereit.
  */
-//require([
-//    'jquery', 'extension', 'config'
-//],function($, extension, config){
-//    var outerBrowserHeight, // TODO Initiale Browserbreite hierüber setzen
-//        innerBrowserHeight,
-//        outerBrowserWidth,
-//        innerBrowserWidth,
-//        mediaList = ["test 1", "test 2"];
-//
-//    /**
-//     * Behandelt die Anzeige der aktuell greifenden Media Angaben.
-//     * @param {{type:string, data:json}} request - Daten und Typ der Anfrage
-//     */
-//    var handleCurrentMediaMessage = function(request){
-//        if(request.type === config.messageTypes.displayCurrentMediaList) {
-//            mediaList = request.data.mediaList;
-//        }
-//    };
-//
-//    extension.handleMessage(handleCurrentMediaMessage);
-//
-//    return {
-//        mediaList: mediaList
-//    };
-//});
+define([
+    'jquery', 'extension', 'config'
+],function($, extension, config){
+    var outerBrowserHeight, // TODO Initiale Browserbreite hierüber setzen
+        innerBrowserHeight,
+        outerBrowserWidth,
+        innerBrowserWidth,
+        mediaList = ["test 1", "test 2"];
 
-var mediaList = ["test"];
+    /**
+     * Behandelt die Anzeige der aktuell greifenden Media Angaben.
+     * @param {{type:string, data:json}} request - Daten und Typ der Anfrage
+     */
+    var handleCurrentMediaMessage = function(request){
+        if(request.type === config.messageTypes.displayCurrentMediaList) {
+            mediaList = request.data.mediaList;
+        }
+    };
+
+    extension.handleMessage(handleCurrentMediaMessage);
+
+    return {
+        mediaList: mediaList
+    };
+});
+
+//var mediaList = ["test"];
