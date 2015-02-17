4e8f19805f14f93c6fbac58c7ae3b25cb3c51d11
ellenschwartau <ellen.schwartau@hotmail.de>
2015-02-17 16:00:38 +0100

    requirejs aufräumen

--- src/js/requirejs/requireContent.js
+++ src/js/requirejs/requireContent.js
@@ -1,13 +1,14 @@
 /**
  * Sorgt dafür, dass die Skripte von Websites richtig geladen werden.
- * @param context
- * @param moduleName
- * @param url
+ * @param {Object} context - Enthält Informationen zum Kontext, wie beispielsweise die geladene RequireJS Konfiguration
+ * @param {string} moduleName - Name des Moduls
+ * @param {string} url - URL zum Modul
  */
 require.load = function(context, moduleName, url) {
     var request = new XMLHttpRequest();
+    // URL zum Modul ausgehend von der Erweiterung abfragen
     request.open("GET", chrome.extension.getURL(url) + "?r=" + (new Date()).getTime(), true);
-    request.onreadystatechange = function(e) {
+    request.onreadystatechange = function() {
         // Request ist abgeschlossen und Status OK
         if(request.readyState === 4 && request.status === 200) {
             eval(request.responseText);
