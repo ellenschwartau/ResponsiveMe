c15a3e784a29961f534dc3ba807952bf1fa381e4
ellenschwartau <ellen.schwartau@hotmail.de>
2015-02-28 16:19:59 +0100

    JSDOC

--- src/js/config.js
+++ src/js/config.js
@@ -6,13 +6,25 @@ define([
  * @returns {{baseDir: string, modules: Array, resolutions: Array, defaultGridColor: string}}
  */
 function() {
-    var baseDir = "modules/",           // Basispfad zu den Modulen
-        modules = [                     // Module, die eingebunden werden sollen
+    /**
+     * Basispfad zu den Source Dateien der Module.
+     * @type {string}
+     */
+    var baseDir = "modules/";
+    /**
+     * Module, die eingebunden werden sollen
+     * @type {Array}
+     */
+    var modules = [
             "viewport",
             "grid",
             "media-queries"
-        ],
-        resolutions = [                 // vordefinierte Display-Auflösungen
+        ];
+    /**
+     * Vordefinierte Display-Auflösungen.
+     * @type {Array}
+     */
+    var resolutions = [
             {name: "1920x1080", width: 1920, height: 1200},
             {name: "1366x768", width: 1366, height: 768},
             {name: "1280x800", width: 1280, height: 800},
@@ -20,8 +32,12 @@ function() {
             {name: "800x600", width: 800, height: 600},
             {name: "640x960", width: 640, height: 960},
             {name: "320x480", width: 320, height: 480}
-        ],
-        defaultGridColor = "#FF8000";   // Default-Farbe zur Visualisierung des Grids
+        ];
+    /**
+     * Default-Farbe zur Visualisierung des Grids
+     * @type {string}
+     */
+    var defaultGridColor = "#FF8000";
 
     return {
         baseDir: baseDir,
